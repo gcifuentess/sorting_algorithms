3d0f78ba82224ada5ae3b3000fbf1419eba754cf
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-21 03:26:35 +0000

    File fixed: 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,6 +15,10 @@ void swap(int *a, int *b, int *array, size_t size);
  */
 void quick_sort(int *array, size_t size)
 {
+
+	if (!array || !size)
+		return;
+
 	quicksort(array, size, 0, size - 1);
 }
 
